7862eb7d70c2be934dbf33a971ee439e6c4265de
leon <leon@chope.co>
2019-03-10 23:48:38 +0800

    delete return insert id

--- application/models/data/D_order.php
+++ application/models/data/D_order.php
@@ -11,13 +11,11 @@ if (!defined('BASEPATH')){
  */
 class d_order extends CI_model
 {
-    var $DB_W = NULL;
-    var $DB_R = NULL;
     var $table_name = 'reservation';
     public function __construct()
     {
         //暂时不会主从库，需要分主从库时再拆分
-        $this->DB_W = $this->DB_R = $this->load->database();
+        $this->load->database();
     }
     
 
@@ -26,8 +24,7 @@ class d_order extends CI_model
         if (empty($data_arr)) {
             return false;
         }
-        $this->db->insert($this->table_name, $data_arr);
-        return $this->db->insert_id();
+        return $this->db->insert($this->table_name, $data_arr);
     }
 
     public function update($where_arr, $update_arr)
@@ -37,15 +34,15 @@ class d_order extends CI_model
         }
         
         // Update
-        $this->DB_W->where($where_arr);
-        $this->DB_W->update($this->table_name, $update_arr);
-        return $this->DB_W->affected_rows();
+        $this->db->where($where_arr);
+        $this->db->update($this->table_name, $update_arr);
+        return $this->db->affected_rows();
     }
 
     public function get_number($where_data)
     {
-        $this->DB_R->where($where_data);
-        $this->DB_R->from($this->table_name);
-        return $this->DB_R->count_all_results();
+        $this->db->where($where_data);
+        $this->db->from($this->table_name);
+        return $this->db->count_all_results();
     }
 }
